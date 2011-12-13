99a1569891b19c5debaf96a0b1f3638b3dd86d6f
Christine Yen <cyen@christineyen.com>
2011-12-12 17:12:57 -0800

    Initial Commit

---
+++ century/CenturyAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CenturyAppDelegate.h
+//  century
+//
+//  Created by Christine Yen on 12/12/11.
+//  Copyright (c) 2011 MIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CenturyAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ century/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  century
+//
+//  Created by Christine Yen on 12/12/11.
+//  Copyright (c) 2011 MIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CenturyAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CenturyAppDelegate class]));
+    }
+}
